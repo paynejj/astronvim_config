97d28bdc8be21928be2c39d292b481ba635044be
paynejj <paynejj@github.com>
2023-06-07 13:59:11 -0400

    SQL configuration is wack

--- mappings.lua
+++ mappings.lua
@@ -11,7 +11,9 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
       end,
       desc = "Pick to close",
     },
@@ -22,6 +24,7 @@ return {
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
   t = {
+    ["<leader><Esc>"] = { "<cmd>ToggleTerm<cr>" },
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
   },
