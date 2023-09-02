38e758f0d5a38b2cefd0c87ef8d3d32e9ef499c4
Joshua Payne <joshua.payne.j@gmail.com>
2023-09-02 12:29:51 -0400

    fix colorcheme

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "shadowfox",
+  colorscheme = "nightfox",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
