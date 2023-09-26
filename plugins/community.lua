4b9cb3c6c250d13c66d707dc8d329296ea336736
paynejj <joshua.payne.j@gmail.com>
2023-09-25 21:35:01 -0400

    Add docker support and fix eslint/prettier

--- plugins/community.lua
+++ plugins/community.lua
@@ -12,6 +12,7 @@ return {
   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.nix" },
   { import = "astrocommunity.pack.python" },
--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -9,10 +9,12 @@ return {
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
     config.sources = {
       -- Set a formatter
-      -- null_ls.builtins.formatting.stylua,
-      -- null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.formatting.prettier,
+      null_ls.builtins.diagnostics.eslint_d,
+      null_ls.builtins.code_actions.eslint_d,
       -- null_ls.builtins.diagnostics.sqlfluff.with {
-      --   extra_args = { "--dialect", "postgres" }, -- change to your dialect
+      -- extra_args = { "--dialect", "postgres" }, -- change to your dialect
       -- },
     }
     return config -- return final config table
