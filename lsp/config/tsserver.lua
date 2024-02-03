680775b9dd334dfc257382120135a8b7ffa81605
Joshua Payne <joshua.payne.j@gmail.com>
2024-02-03 15:22:39 -0500

    disable tsserver formatting the cool way

---
+++ lsp/config/tsserver.lua
@@ -0,0 +1,14 @@
+return {
+  settings = {
+    javascript = {
+      format = {
+        enable = false,
+      },
+    },
+    typescript = {
+      format = {
+        enable = false,
+      },
+    },
+  },
+}
