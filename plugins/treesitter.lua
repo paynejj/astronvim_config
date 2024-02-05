74507ba00086036a4ccde5a127f4a7c92ac80a0e
Joshua Payne <joshua.payne.j@gmail.com>
2024-02-04 20:09:33 -0500

    configure neotree and treesitter

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,9 +1,31 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { "c", "lua", "rust" },
-    highlight = {
-      disable = { "sql" },
+    -- Installed by astro
+    -- bash, c , lua, markdown, markdown_inline, python, query, vim, vimdoc
+    ensure_installed = {
+      "awk",
+      "css",
+      "dockerfile",
+      "git_config",
+      "git_rebase",
+      "gitattributes",
+      "gitcommit",
+      "gitignore",
+      "html",
+      "java",
+      "javascript",
+      "jsdoc",
+      "json",
+      "jsonc",
+      "kdl",
+      "nix",
+      "regex",
+      "toml",
+      "tsx",
+      "typescript",
+      "xml",
+      "yaml",
     },
   },
 }
